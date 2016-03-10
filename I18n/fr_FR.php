0262266e06530ee425a7edcbfa80caa3112fc01a
Pradat <tom.pradat@gmail.com>
2016-03-10 10:56:38 +0100

    traductions

---
+++ I18n/fr_FR.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+    'The brand id %id doesn\'t exist' => 'La marque d\'id %id n\'existe pas',
+    'The category id %id doesn\'t exist' => 'La rubrique d\'id %id n\'existe pas',
+    'The content id %id doesn\'t exist' => 'Le contenu d\'id %id n\'existe pas',
+    'The folder id %id doesn\'t exist' => 'La dossier d\'id %id n\'existe pas',
+    'The product ref %ref doesn\'t exist' => 'La produit ayant la référence %ref n\'existe pas',
+);
